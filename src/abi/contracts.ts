1edcbc925f25cd8a9f26d02d86cf2baa1e867e36
Semi Invader <=>
2023-09-08 23:51:59 -0600

    updated contract pointer

--- src/abi/contracts.ts
+++ src/abi/contracts.ts
@@ -1,3 +1,3 @@
-export const fountain = "0xaca83A3012b21721e69D0730Ed507C5bF253Ee57"
+export const fountain = "0x779Ebc8fb6F92EB0b19DcFC558c4Cb849f32640a"
 export const hades = "0x7f3ea5f3bbfb7a0accf6df8c7ed672caf9212317"
 export const scepter = "0x3788f0c29450df6a3684706a6bfc98b5e188fe4e"
