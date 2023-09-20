91cc15d88ea88be27e39c65747317d13017b0cd3
Semi Invader <=>
2023-09-19 22:51:12 -0600

    troops page

--- src/app/troops/page.tsx
+++ src/app/troops/page.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from "next";
 
 const TroopsPage: NextPage = () => {
   return (
-    <div>
-      <h1>Troops</h1>
-    </div>
+    <main className="flex flex-col items-center w-full p-12">
+      <div className="container">
+        <h1 className="text-center text-3xl font-greek cool-text">Troops</h1>
+      </div>
+    </main>
   );
 };
 
