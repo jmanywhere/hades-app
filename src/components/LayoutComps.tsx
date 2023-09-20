dd5f830eea5c5c2dca1fca8de6b8de0294a1ae39
Semi Invader <=>
2023-09-19 23:03:53 -0600

    fix header not allowing click on nav

--- src/components/LayoutComps.tsx
+++ src/components/LayoutComps.tsx
@@ -23,59 +23,59 @@ export const Header = () => {
         <div className="font-dalek text-5xl md:text-5xl cool-text pr-6">
           Hades
         </div>
-        <nav className="hidden navbar-center font-gideon_roman uppercase text-xl lg:flex flex-row items-center gap-6">
-          <Link
-            href="/"
-            className={classNames(
-              pathname === "/" ? "hot-text" : "",
-              "hover:cool-text hover:opacity-70 transition-all duration-500 "
-            )}
-          >
-            Home
-          </Link>
-          <Link
-            href="/"
-            className={classNames(
-              pathname === "/swap" ? "hot-text" : "",
-              "hover:cool-text hover:opacity-70 transition-all duration-500 "
-            )}
-          >
-            Swap
-          </Link>
-          <Link
-            href="/fountain"
-            className={classNames(
-              pathname === "/fountain" ? "hot-text" : "",
-              "hover:cool-text hover:opacity-70 transition-all duration-500 "
-            )}
-          >
-            fountain
-          </Link>
-          <Link
-            href="/"
-            className={classNames(
-              pathname === "/underworld" ? "hot-text" : "",
-              "hover:cool-text hover:opacity-70 transition-all duration-500 "
-            )}
-          >
-            underworld
-          </Link>
-          <Link
-            href="/troops"
-            className={classNames(
-              pathname === "/troops" ? "hot-text" : "",
-              "hover:cool-text hover:opacity-70 transition-all duration-500 "
-            )}
-          >
-            Troops
-          </Link>
-        </nav>
       </div>
-      <div className="navbar-end gap-x-4">
+      <nav className="hidden font-gideon_roman uppercase text-xl lg:flex flex-row items-center gap-6">
+        <Link
+          href="/"
+          className={classNames(
+            pathname === "/" ? "hot-text" : "",
+            "hover:cool-text hover:opacity-70 transition-all duration-500 "
+          )}
+        >
+          Home
+        </Link>
+        <Link
+          href="/"
+          className={classNames(
+            pathname === "/swap" ? "hot-text" : "",
+            "hover:cool-text hover:opacity-70 transition-all duration-500 "
+          )}
+        >
+          Swap
+        </Link>
+        <Link
+          href="/fountain"
+          className={classNames(
+            pathname === "/fountain" ? "hot-text" : "",
+            "hover:cool-text hover:opacity-70 transition-all duration-500 "
+          )}
+        >
+          fountain
+        </Link>
+        <Link
+          href="/"
+          className={classNames(
+            pathname === "/underworld" ? "hot-text" : "",
+            "hover:cool-text hover:opacity-70 transition-all duration-500 "
+          )}
+        >
+          underworld
+        </Link>
+        <Link
+          href="/troops"
+          className={classNames(
+            pathname === "/troops" ? "hot-text" : "",
+            "hover:cool-text hover:opacity-70 transition-all duration-500 "
+          )}
+        >
+          Troops
+        </Link>
+      </nav>
+      <div className="navbar-end">
         <div className="hidden md:block">
           <Web3Button icon="hide" balance="hide" />
         </div>
-        <button className="lg:hidden btn bg-secondary/20 hover:bg-secondary/40">
+        <button className="lg:hidden btn bg-secondary/20 hover:bg-secondary/40 ml-4">
           <CgMenuRightAlt className="text-3xl text-white" />
         </button>
       </div>
